623c4f375272ad664ce95a98d50fe1d45e6b5c5a
mounaayed <mouna.ayed@esprit.tn>
2021-11-21 21:23:01 +0100

    "integration template"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { LayoutComponent } from './home/layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home', component: HomeComponent, children: [
+      {path: '', component: LayoutComponent},]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { FooterComponent } from './home/footer/footer.component';
+import { NavbarComponent } from './home/navbar/navbar.component';
+import { SidbarComponent } from './home/sidbar/sidbar.component';
+import { LayoutComponent } from './home/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidbarComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule,
