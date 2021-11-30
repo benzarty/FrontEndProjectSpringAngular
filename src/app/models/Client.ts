691de07a65debbba144bc52bab9511b428beb5f1
Nassim Ben Aissa <nassim.benaissa@esprit.tn>
2021-11-30 01:43:49 +0100

    Commit first try
    Just adding client model

---
+++ src/app/models/Client.ts
@@ -0,0 +1,10 @@
+export class Client{
+    id_client?: number;
+    categorie_client?: string;
+    nom?: string;
+    prenom?: string;
+    email?: string;
+    password?: string;
+    profession?: string;
+    date_naissance?: string;
+    }
